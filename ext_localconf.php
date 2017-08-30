13ad5ec8325d759fe5d30d0fd7eaceb7bb6be07f
areimund <andreas.reimund@online.de>
2017-08-30 17:28:00 +0200

    Add files via upload

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,7 +1,6 @@
 <?php
 if (!defined ('TYPO3_MODE')) 	die ('Access denied.');
-
-Tx_Extbase_Utility_Extension::configurePlugin(
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
 	$_EXTKEY,
 	'Display',
 	array(
@@ -12,7 +11,7 @@ Tx_Extbase_Utility_Extension::configurePlugin(
 	)
 );
 
-require t3lib_extMgm::extPath($_EXTKEY) . 'Configuration/RealURL/default.php';
+require \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'Configuration/RealURL/default.php';
 
 $GLOBALS ['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass']['podcast'] = 'EXT:podcast/Classes/Utilities/Backend/EpisodePostProcessor.php:Tx_Podcast_Utilities_EpisodePostProcessor';
 
